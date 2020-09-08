670fb0591f3b06aa4328a65d58bcc8d264cb7171
Weiyuan Liu <weiyuan.lane@gmail.com>
2020-09-08 19:41:31 +0800

    [Feature] Add stats retriever

--- scripts/get-jobs-dump.js
+++ scripts/get-jobs-dump.js
@@ -1,7 +1,8 @@
 const PAGE_LIMIT = 100;
 const PAGE_START_AT = 1;
+const ERROR_TOLERANCE = 10;
 const PAGE_URL = 'https://api.mycareersfuture.gov.sg/v2/jobs?sortBy=new_posting_date&omitCountWithSchemes=true';
-const WRITE_DATA_PATH = "dumpdata"
+const WRITE_DATA_PATH = 'dumpdata';
 
 const https = require('https');
 const fs = require('fs');
@@ -9,6 +10,7 @@ const fs = require('fs');
 let hasMoreResults = true;
 let page = PAGE_START_AT;
 let allData = [];
+let errorCount = 0;
 
 async function getDataLoop() {
     return new Promise((resolve, reject) => {
@@ -20,8 +22,12 @@ async function getDataLoop() {
             });
 
             res.on('end', function(){
-                const parsedData = JSON.parse(body);
-                resolve(parsedData.results);
+                try {
+                    const parsedData = JSON.parse(body);
+                    resolve(parsedData.results);
+                } catch (e) {
+                    reject(e);
+                }
             });
         }).on('error', (e) => {
             reject(e);
@@ -32,17 +38,51 @@ async function getDataLoop() {
 async function main() {
     let currentData = [];
     while (hasMoreResults) {
-        currentData = await getDataLoop();
+        console.log(`Getting page #${page}`);
+        try {
+            currentData = await getDataLoop();
+        }
+        catch (e) {
+            errorCount++;
+
+            console.log(`Encountered "getDataLoop" error ${errorCount}`, e);
+            if (errorCount > ERROR_TOLERANCE) {
+                console.log('Error tolerance exceeded, exiting');
+                break;
+            } else {
+                console.log('Error tolerance not exceeded, retrying');
+                continue;
+            }
+        }
+
+        // Cleanup, remove redundant data
+        const processedData = currentData.map((entity) => {
+            let processedEntity = entity;
+            delete processedEntity.description;
+
+            if (processedEntity && processedEntity.postedCompany && typeof(processedEntity.postedCompany.description) !== 'undefined') {
+                delete processedEntity.postedCompany.description;
+            }
+            if (processedEntity && processedEntity.hiringCompany && typeof(processedEntity.hiringCompany.description) !== 'undefined') {
+                delete processedEntity.hiringCompany.description;
+            }
+
+            return processedEntity;
+        });
 
         if (currentData.length > 0) {
-            allData = allData.concat(currentData);
+            allData = allData.concat(processedData);
             page++;
         } else {
             hasMoreResults = false;
         }
+
+        if (page > 5) {
+            break;
+        }
     }
 
-    fs.writeFile(WRITE_DATA_PATH, allData, (err) => {
+    fs.writeFile(WRITE_DATA_PATH, JSON.stringify(allData), (err) => {
         if(err) {
             return console.log(err);
         }
---
+++ scripts/get-stats.js
@@ -0,0 +1,184 @@
+
+const WRITE_DATA_PATH = 'dumpdata';
+
+/**
+ *  All valid employment types:
+ *
+ * 'Permanent'
+ * 'Full Time'
+ * 'Part Time'
+ * 'Contract'
+ * 'Flexi-work'
+ * 'Temporary'
+ * 'Freelance'
+ * 'Internship/Traineeship'
+ */
+const EMPLOYMENT_TYPES = [
+    'Permanent',
+    'Full Time',
+];
+
+/**
+ *  All valid job levels:
+ *
+ * 'Senior Management'
+ * 'Middle Management'
+ * 'Manager'
+ * 'Professional'
+ * 'Senior Executive'
+ * 'Executive'
+ * 'Junior Executive'
+ * 'Non-executive'
+ * 'Fresh/entry level'
+ */
+
+const JOB_LEVELS = [
+    'Executive',
+];
+
+
+/**
+ * All valid skills to be filtered against
+ */
+
+const NEEDED_SKILLS = [
+    'Software Development',
+    'Software Engineering',
+];
+
+/**********/
+
+const fs = require('fs');
+
+const filters = [
+    { entityKey: 'positionLevels', propKey: 'position', filterVals: JOB_LEVELS },
+    // { entityKey: 'employmentType', propKey: 'employmentType', filterVals: EMPLOYMENT_TYPES },
+    // { entityKey: 'skills', propKey: 'skill', filterVals: NEEDED_SKILLS },
+]
+
+function isEntityFoundFrom(entityData, filter) {
+    let found = false;
+
+    if (entityData[filter.entityKey]) {
+        const filterPropList = entityData[filter.entityKey];
+
+        found = filterPropList.some((filterProp) => {
+            return filter.filterVals.includes(filterProp[filter.propKey]);
+        });
+    }
+
+    return found;
+}
+
+function matchEntitiesFoundFrom(entitesData, filter) {
+    const found = entitesData.filter((entityData) => {
+        return isEntityFoundFrom(entityData, filter);
+    });
+
+    return found;
+}
+
+function searchData(data) {
+    let filteredData = data;
+
+    filters.forEach((filter) => {
+        filteredData = matchEntitiesFoundFrom(filteredData, filter);
+    });
+
+    return filteredData;
+}
+
+function getNumericalStatsFrom(numberList) {
+    if (numberList.length === 0) {
+        return null
+    }
+
+    let lowest = numberList[0];
+    let highest = numberList[0];
+    let total = 0;
+    const numberMap = {};
+
+    numberList.forEach((number) => {
+        if (number < lowest) {
+            lowest = number;
+        }
+
+        if (number > highest) {
+            highest = number;
+        }
+
+        total += number;
+
+        if (typeof(numberMap[number]) !== 'undefined') {
+            numberMap[number]++;
+        } else {
+            numberMap[number] = 0;
+        }
+    });
+
+    const mean = Math.ceil(total / numberList.length);
+    const numberMapKeys = Object.keys(numberMap);
+    const mode = numberMapKeys.reduce((foundKey, currentKey) => {
+        if (numberMap[foundKey] > numberMap[currentKey]) {
+            return foundKey;
+        }
+
+        return currentKey;
+    }, numberMapKeys[0]);
+
+    return {
+        lowest,
+        highest,
+        mean,
+        mode,
+    }
+}
+
+function getSalaryStatsFrom(entitiesData) { 
+    const minimumList = entitiesData.map((entityData) => {
+        return entityData.salary.minimum;
+    });
+    const maximumList = entitiesData.map((entityData) => {
+        return entityData.salary.maximum;
+    });
+    const middleList = entitiesData.map((entityData) => {
+        const middle = (entityData.salary.maximum - entityData.salary.minimum) / 2 + entityData.salary.minimum;
+        return Math.ceil(middle);
+    });
+
+    return {
+        minimum: getNumericalStatsFrom(minimumList),
+        maximum: getNumericalStatsFrom(maximumList),
+        middle: getNumericalStatsFrom(middleList),
+    }
+}
+
+function process(data) {
+    let processedContent = {};
+
+    processedContent.all = {
+        length: data.length,
+    };
+
+    const filteredData = searchData(data);
+    processedContent.filtered = {
+        length: filteredData.length,
+        stats: getSalaryStatsFrom(filteredData),
+    }
+
+    return processedContent;
+}
+
+async function main() {
+    fs.readFile(WRITE_DATA_PATH, (err, data) => {
+        if (err) {
+            console.log(err);
+        } else {
+            const parsedData = JSON.parse(data);
+            const stats = process(parsedData); 
+            console.log(JSON.stringify(stats));
+        }
+    });
+}
+
+main();
